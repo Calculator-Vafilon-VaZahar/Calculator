e824b4024ba81699abd60d04f36e26a53831847f
Vanhelsing135 <vanbest27@gmail.com>
2022-12-29 13:20:08 +0300

    Добавлена функция root, перевод в 2 с/с, добавлена сортировка элементов

--- calculator/calculator/functions.cpp
+++ calculator/calculator/functions.cpp
@@ -1,62 +1,96 @@
 #include "functions.h"
 #include<iostream>
+
 using namespace std;
+
+string TypeOfCalculator() {
+    int a;
+    cout << "0-Exit" << endl;
+    cout << "1-Calculator" << endl;
+    cout << "2-Sorting" << endl;
+    cout << "3-binary view" << endl;
+    cin >> a;
+    while (cin.fail() || (a != 0 and a != 1 and a != 2 and a != 3)) {
+        cin.clear();
+        cin.ignore();
+        cout << "Error. a must be 0 - 3" << endl;
+        cout << "0-Exit" << endl;
+        cout << "1-Calculator" << endl;
+        cout << "2-Sorting" << endl;
+        cout << "3-binary view" << endl;
+        cin >> a;
+    }
+    switch (a) {
+    case 0: return "exit";
+        break;
+    case 1: return "standart calculator";
+        break;
+    case 2: return "sorting";
+        break;
+    case 3: return "binary view";
+        break;
+    }
+    return "Error";
+}
 Operation askForOperation() {
     int num_of_operation;
     cout << "Choose the operation: " << endl;
     cout << "Arithmetics:" << "\t\t\t\t\t" << "Trigonometry:" << endl;
-    cout << "1 - Add" << "\t\t\t\t\t\t\t" << "12 - Sinus" << endl;
-    cout << "2 - Subtract" << "\t\t\t\t\t" << "13 - Cosinus"  << endl;
-    cout << "3 - Multiply" << "\t\t\t\t\t" << "14 - Tangent" << endl;
-    cout << "4 - Divide" << "\t\t\t\t\t\t" << "15 - Cotangent" << endl;
+    cout << "1 - Add" << "\t\t\t\t\t\t" << "13 - Sinus" << endl;
+    cout << "2 - Subtract" << "\t\t\t\t\t" << "14 - Cosinus" << endl;
+    cout << "3 - Multiply" << "\t\t\t\t\t" << "15 - Tangent" << endl;
+    cout << "4 - Divide" << "\t\t\t\t\t" << "16 - Cotangent" << endl;
     cout << "5 - Unary_minus" << endl;
     cout << "6 - Exp" << endl;
     cout << "7 - Log" << endl;
     cout << "8 - Sqrt" << endl;
-    cout << "9 - Pow" << endl;
+    cout << "9 - Power" << endl;
     cout << "10 - Remainder" << endl;
     cout << "11 - unary_plus" << endl;
+    cout << "12 - Root" << endl;
     cout << "0 - exit" << endl;
     cin >> num_of_operation;
-    while (cin.fail() || num_of_operation < 0 || num_of_operation > 20) {
+    while (cin.fail() || num_of_operation < 0 || num_of_operation > 16) {
         cin.clear();
         cin.ignore();
-        cout << "enter the number of operation(1-13):  ";
+        cout << "enter the number of operation(0-16):  ";
         cin >> num_of_operation;
     }
     switch (num_of_operation) {
-        case 1: return Operation::add;
-            break;
-        case 2: return Operation::subtract;
-            break;
-        case 3: return Operation::multiply;
-            break;
-        case 4: return Operation::divide;
-            break;
-        case 5: return Operation::unary_minus;
-            break;
-        case 6: return Operation::Exp;
-            break;
-        case 7: return Operation::Log;
-            break;
-        case 8: return Operation::Sqrt;
-            break;
-        case 9: return Operation::Pow;
-            break;
-        case 10: return Operation::Remainder;
-            break;
-        case 11: return Operation::unary_plus;
-            break;
-        case 12: return Operation::Sinus;
-            break;
-        case 13: return Operation::Cosinus;
-            break;
-        case 14: return Operation::Tangent;
-            break;
-        case 15: return Operation::Cotangent;
-            break;
-        case 0: return Operation::Exit;
-            break;
+    case 1: return Operation::add;
+        break;
+    case 2: return Operation::subtract;
+        break;
+    case 3: return Operation::multiply;
+        break;
+    case 4: return Operation::divide;
+        break;
+    case 5: return Operation::unary_minus;
+        break;
+    case 6: return Operation::Exp;
+        break;
+    case 7: return Operation::Log;
+        break;
+    case 8: return Operation::Sqrt;
+        break;
+    case 9: return Operation::Power;
+        break;
+    case 10: return Operation::Remainder;
+        break;
+    case 11: return Operation::unary_plus;
+        break;
+    case 12: return Operation::Root;
+        break;
+    case 13: return Operation::Sinus;
+        break;
+    case 14: return Operation::Cosinus;
+        break;
+    case 15: return Operation::Tangent;
+        break;
+    case 16: return Operation::Cotangent;
+        break;
+    case 0: return Operation::Exit;
+        break;
     }
     return Operation::Exit;
 }
@@ -88,14 +122,14 @@ int Unary_plus(int a) {
 }
 double fact(int N)
 {
-    if(N < 0)
+    if (N < 0)
         return 0;
     if (N == 0)
         return 1;
     else
         return N * fact(N - 1);
 }
-double sinus(double x){
+double sinus(double x) {
     long double e = (1e-20);
     long double ch = x;
     long double zn = 1;
@@ -116,41 +150,41 @@ double sinus(double x){
     }
     return res;
 }
-double cosinus(double x){
-    return sqrt((1-(pow(sinus(x),2))));
+double cosinus(double x) {
+    return sqrt((1 - (pow(sinus(x), 2))));
 }
-double tangent(double x){
-    return sinus(x)/ cosinus(x);
+double tangent(double x) {
+    return sinus(x) / cosinus(x);
 }
-double cotangent(double x){
-    return cosinus(x)/ sinus(x);
+double cotangent(double x) {
+    return cosinus(x) / sinus(x);
 }
 
 double applyUnaryOperation(Operation oper, int x) {
     switch (oper) {
-        case Operation::Exp: return exp(x);
-            break;
-        case Operation::Sqrt: return sqrt(x);
-            break;
-        case Operation::Log: return log(x);
-            break;
-        case Operation::unary_minus: return Unary_minus(x);
-            break;
-        case Operation::unary_plus: return Unary_plus(x);
-            break;
-        case Operation::Sinus: return sinus(x);
-            break;
-        case Operation::Cosinus: return cosinus(x);
-            break;
-        case Operation::Tangent: return tangent(x);
-            break;
-        case Operation::Cotangent: return cotangent(x);
-            break;
+    case Operation::Exp: return exp(x);
+        break;
+    case Operation::Sqrt: return sqrt(x);
+        break;
+    case Operation::Log: return log(x);
+        break;
+    case Operation::unary_minus: return Unary_minus(x);
+        break;
+    case Operation::unary_plus: return Unary_plus(x);
+        break;
+    case Operation::Sinus: return sinus(x);
+        break;
+    case Operation::Cosinus: return cosinus(x);
+        break;
+    case Operation::Tangent: return tangent(x);
+        break;
+    case Operation::Cotangent: return cotangent(x);
+        break;
     }
     return 0;
 }
 
-double Sum(double a, double b){
+double Sum(double a, double b) {
     return a + b;
 }
 
@@ -191,24 +225,94 @@ int Mod(int a, int b) {
     return a % b;
 }
 
-double applyBinaryOperation(Operation oper,int a,int b) {
+double power(double a, double b) {
+    return pow(a, b);
+}
+
+double root(double a, double b) {
+    try {
+        return pow(a, 1 / b);
+    }
+    catch (...) {
+        cout << "ERROR!!! Check your data!" << endl;
+    }
+}
+
+double applyBinaryOperation(Operation oper, int a, int b) {
     switch (oper) {
-        case Operation::subtract: return Sub(a, b);
-            break;
-        case Operation::multiply: return Mult(a, b);
-            break;
-        case Operation::Pow: return pow(a,b);
-            break;
-        case Operation::divide: return Div(a, b);
-            break;
-        case Operation::add: return Sum(a, b);
-            break;
-        case Operation::Remainder: return Mod(a, b);
-            break;
+    case Operation::subtract: return Sub(a, b);
+        break;
+    case Operation::multiply: return Mult(a, b);
+        break;
+    case Operation::Power: return pow(a, b);
+        break;
+    case Operation::divide: return Div(a, b);
+        break;
+    case Operation::add: return Sum(a, b);
+        break;
+    case Operation::Remainder: return Mod(a, b);
+        break;
+    case Operation::Root: return root(a, b);
     }
     return 0;
 }
 
+void bubbleSort(vector <double> vect)
+{
+    int size = vect.size();
+    int tmp = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = (size - 1); j >= (i + 1); j--) {
+            if (vect[j] < vect[j - 1]) {
+                tmp = vect[j];
+                vect[j] = vect[j - 1];
+                vect[j - 1] = tmp;
+            }
+        }
+    }
+}
+
+void CreatingSortingMassive() {
+    vector<double>elements;
+    int size;
+    cout << "enter the amount of elements:" << endl;
+    cin >> size;
+    for (int i = 0; i < size; i++) {
+        double a;
+        cout << " enter the element: " << endl;
+        cin >> a;
+        elements.push_back(a);
+    }
+    int tmp = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = (size - 1); j >= (i + 1); j--) {
+            if (elements[j] < elements[j - 1]) {
+                tmp = elements[j];
+                elements[j] = elements[j - 1];
+                elements[j - 1] = tmp;
+            }
+        }
+    }
+    cout << "Sorted elements: " << endl;
+    for (int i = 0; i < size; i++) {
+        cout << elements[i] << "\t";
+    }
+    cout << endl;
+}
+
+
+void DecToBinary() {
+    int num;
+    cin >> num;
+    int bin = 0;
+    int k = 1;
+    while (num) {
+        bin += (num % 2) * k;
+        k *= 10;
+        num /= 2;
+    }
+    cout << bin << endl;
+}
 
 void printResult(double res) {
     cout << res << endl;
--- calculator/calculator/functions.h
+++ calculator/calculator/functions.h
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <cmath>
-// Ваня хуй
-// Add trigonometry
+#include<vector>
+
 using namespace std;
 enum Operation {
-    //arithmetical
+	//arithmetical
 	add = 1,
 	subtract = 2,
 	multiply = 3,
-	divide = 4, 
+	divide = 4,
 	Exp = 6, //not arithmetic
 	Log = 7, // not arithmetic
 	Sqrt = 8,//not arithmetic
-	Pow = 9, //not arithmetic
+	Power = 9, //not arithmetic
 	Exit = 0,
 	Remainder = 10,
 	unary_plus = 11,
 	unary_minus = 5,
-    // trigonometry
-    Sinus = 12,
-    Cosinus = 13,
-    Tangent = 14,
-    Cotangent = 15,
+	Root = 12,
+	// trigonometry
+	Sinus = 13,
+	Cosinus = 14,
+	Tangent = 15,
+	Cotangent = 16,
 };
 // arithmetical functions
 double Sum(double a, double b);
@@ -29,19 +30,104 @@ double Sub(double a, double b);
 double Mult(double a, double b);
 double Div(double a, double b);
 int Mod(int a, int b);
-double Root(double a, double b);
-double Power(double a, double b);
+double root(double a, double b);
+double power(double a, double b);
 Operation askForOperation();
 bool isUnary(Operation);
 int askForNumber();
 double applyUnaryOperation(Operation oper, int x);
 double applyBinaryOperation(Operation oper, int a, int b);
-void printResult(int res);
+void printResult(double res);
 int Unary_minus(int a);
 int Unary_plus(int a);
 double cotangent(double x);
 double tangent(double x);
 double sinus(double x);
 double cosinus(double x);
+void bubbleSort(vector<double> vect);
+void CreatingSortingMassive();
+void DecToBinary();
+
+
+
+// c/c root power
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
